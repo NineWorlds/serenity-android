b12b25bbcf50f1f355ec9ed49d105367f89f5b83
David Carver <d_a_carver@yahoo.com>
2012-12-29 17:35:28 -0500

    Add utility methods to retrieve some static resource paths.

---
+++ PlexAppRESTLibrary/src/test/java/com/github/kingargyle/plexapp/TestResourcePaths.java
@@ -0,0 +1,111 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2012 David Carver
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.kingargyle.plexapp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.kingargyle.plexapp.config.IConfiguration;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author dcarver
+ *
+ */
+public class TestResourcePaths {
+
+	private IConfiguration config;
+	private ResourcePaths path;
+	@Before
+	public void setUp() throws Exception {
+		
+		config = new MockConfiguration();
+		path = new ResourcePaths(config);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		config = null;
+		path = null;
+		
+	}
+	
+	@Test
+	public void testLibraryPath() throws Exception {
+		String result = path.getLibraryURL();
+		
+		assertEquals("http://localhost:32400/library/", result);
+	}
+	
+	@Test
+	public void testSectionsPath() throws Exception {
+		String result = path.getSectionsURL();
+		
+		assertEquals("http://localhost:32400/library/sections/", result);
+	}
+	
+	@Test
+	public void testSectionPathWithKey() throws Exception {
+		String result = path.getSectionsURL("4");
+		
+		assertEquals("http://localhost:32400/library/sections/4/", result);
+	}
+	
+	private class MockConfiguration implements IConfiguration {
+
+		/* (non-Javadoc)
+		 * @see com.github.kingargyle.plexapp.config.IConfiguration#getHost()
+		 */
+		public String getHost() {
+			
+			return "localhost";
+		}
+
+		/* (non-Javadoc)
+		 * @see com.github.kingargyle.plexapp.config.IConfiguration#setHost(java.lang.String)
+		 */
+		public void setHost(String host) {
+			
+		}
+
+		/* (non-Javadoc)
+		 * @see com.github.kingargyle.plexapp.config.IConfiguration#getPort()
+		 */
+		public String getPort() {
+			return "32400";
+		}
+
+		/* (non-Javadoc)
+		 * @see com.github.kingargyle.plexapp.config.IConfiguration#setPort(java.lang.String)
+		 */
+		public void setPort(String port) {
+			
+		}
+		
+	}
+	
+	
+}
