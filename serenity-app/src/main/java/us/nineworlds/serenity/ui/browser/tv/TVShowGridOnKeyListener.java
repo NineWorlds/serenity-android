bd5d88638ef2ffb4250628f810e532ce8102e2f8
David Carver <d_a_carver@yahoo.com>
2014-02-04 18:45:13 -0500

    Additional tweak for game controlers for Grid View

--- serenity-app/src/main/java/us/nineworlds/serenity/ui/browser/tv/TVShowGridOnKeyListener.java
+++ serenity-app/src/main/java/us/nineworlds/serenity/ui/browser/tv/TVShowGridOnKeyListener.java
@@ -1,26 +1,35 @@
-// Copyright 2013 Google Inc. All Rights Reserved.
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2014 David Carver and others 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package us.nineworlds.serenity.ui.browser.tv;
 
 import android.app.Activity;
-import android.os.AsyncTask;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Toast;
 import com.jess.ui.TwoWayAdapterView;
 import us.nineworlds.plex.rest.PlexappFactory;
-import us.nineworlds.plex.rest.model.impl.Directory;
-import us.nineworlds.plex.rest.model.impl.MediaContainer;
-import us.nineworlds.plex.rest.model.impl.Video;
-import us.nineworlds.serenity.R;
 import us.nineworlds.serenity.SerenityApplication;
 import us.nineworlds.serenity.core.model.SeriesContentInfo;
-import us.nineworlds.serenity.core.model.impl.EpisodeMediaContainer;
-import us.nineworlds.serenity.core.model.impl.EpisodePosterInfo;
-import us.nineworlds.serenity.ui.util.VideoPlayerIntentUtils;
-
-import java.util.List;
 
 public class TVShowGridOnKeyListener implements View.OnKeyListener {
 	final PlexappFactory factory = SerenityApplication.getPlexFactory();
@@ -32,7 +41,7 @@ public class TVShowGridOnKeyListener implements View.OnKeyListener {
 
 	@Override
 	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		if (keyCode != KeyEvent.KEYCODE_MEDIA_PLAY || event.getAction() == KeyEvent.ACTION_DOWN) {
+		if ((keyCode != KeyEvent.KEYCODE_MEDIA_PLAY && keyCode != KeyEvent.KEYCODE_BUTTON_R1) || event.getAction() == KeyEvent.ACTION_DOWN) {
 			return false;
 		}
 
