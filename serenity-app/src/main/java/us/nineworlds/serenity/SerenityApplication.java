1c9844cb05bac06cc8011be28a1fd41b4f4ce543
David Carver <d_a_carver@yahoo.com>
2014-05-07 10:38:27 -0400

    Amazon: RejectedExecutionException in Universal Imageloader.
    Removed task executor definitions to fix issues with crashing on Amazon Fire TV
    when downloading images.  Use the default executors instead of trying to use
    the AsyncTask executors.  This is left over from a previous configuration.

--- serenity-app/src/main/java/us/nineworlds/serenity/SerenityApplication.java
+++ serenity-app/src/main/java/us/nineworlds/serenity/SerenityApplication.java
@@ -39,7 +39,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.widget.ImageView;
@@ -65,187 +64,23 @@ import com.squareup.okhttp.OkHttpClient;
 public class SerenityApplication extends Application {
 
 	private static final String COM_GOOGLE_ANDROID_TV = "com.google.android.tv";
-	protected static PlexappFactory plexFactory;
-	private static ConcurrentHashMap<String, Server> plexmediaServers = new ConcurrentHashMap<String, Server>();
-	private static LinkedList<VideoContentInfo> videoQueue = new LinkedList<VideoContentInfo>();
+	private static boolean enableTracking = true;
 	private static ImageLoader imageLoader;
-	public static final int PROGRESS = 0xDEADBEEF;
+	private static DisplayImageOptions movieOptions;
+	private static DisplayImageOptions musicOptions;
 	private static List<PendingDownload> pendingDownloads;
+	protected static PlexappFactory plexFactory;
 
-	public static List<PendingDownload> getPendingDownloads() {
-		return pendingDownloads;
-	}
+	private static ConcurrentHashMap<String, Server> plexmediaServers = new ConcurrentHashMap<String, Server>();
+
+	public static final int PROGRESS = 0xDEADBEEF;
 
 	private static DisplayImageOptions reflectiveOptions;
 
-	public static DisplayImageOptions getReflectiveOptions() {
-		return reflectiveOptions;
-	}
+	private static LinkedList<VideoContentInfo> videoQueue = new LinkedList<VideoContentInfo>();
 
-	private static DisplayImageOptions musicOptions;
-
-	public static DisplayImageOptions getMusicOptions() {
-		return musicOptions;
-	}
-
-	private static boolean enableTracking = true;
-
-	private static DisplayImageOptions movieOptions;
-
-	public static DisplayImageOptions getMovieOptions() {
-		return movieOptions;
-	}
-
-	/**
-	 * 
-	 */
-	public SerenityApplication() {
-		pendingDownloads = new ArrayList<PendingDownload>();
-
-	}
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		init();
-		PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
-		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		SharedPreferences.Editor editor = prefs.edit();
-		if (isGoogleTV(this) || isAndroidTV(this)) {
-			editor.putBoolean("serenity_tv_mode", true);
-			editor.apply();
-		}
-	}
-
-	/**
-	 * 
-	 */
-	public void init() {
-		configureImageLoader();
-		initializePlexappFactory();
-		if (enableTracking) {
-			installAnalytics();
-		}
-		sendStartedApplicationEvent();
-	}
-
-	protected void configureImageLoader() {
-		DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
-				.cacheInMemory(true).cacheOnDisc(true)
-				.bitmapConfig(Bitmap.Config.RGB_565)
-				.resetViewBeforeLoading(true)
-				.showImageForEmptyUri(R.drawable.default_video_cover).build();
-
-		musicOptions = new DisplayImageOptions.Builder().cacheInMemory(true)
-				.cacheOnDisc(true).bitmapConfig(Bitmap.Config.RGB_565)
-				.showImageForEmptyUri(R.drawable.default_music).build();
-
-		movieOptions = new DisplayImageOptions.Builder().cacheInMemory(true)
-				.cacheOnDisc(true).bitmapConfig(Bitmap.Config.RGB_565)
-				.resetViewBeforeLoading(true).build();
-
-		reflectiveOptions = new DisplayImageOptions.Builder()
-				.cacheInMemory(true).cacheOnDisc(true)
-				.bitmapConfig(Bitmap.Config.RGB_565)
-				.displayer(new RoundedBitmapDisplayer(10)).build();
-
-		ImageLoaderConfiguration imageLoaderconfig = new ImageLoaderConfiguration.Builder(
-				this)
-				.taskExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-				.taskExecutorForCachedImages(AsyncTask.THREAD_POOL_EXECUTOR)
-				.threadPoolSize(5)
-				.tasksProcessingOrder(QueueProcessingType.FIFO)
-				.imageDownloader(
-						new OKHttpImageLoader(this, new OkHttpClient()))
-				.defaultDisplayImageOptions(defaultOptions).build();
-
-		imageLoader = ImageLoader.getInstance();
-		imageLoader.init(imageLoaderconfig);
-	}
-
-	/**
-	 * @param deviceModel
-	 */
-	protected void sendStartedApplicationEvent() {
-		String deviceModel = android.os.Build.MODEL;
-		if (enableTracking) {
-			Tracker tracker = EasyTracker.getTracker();
-			if (tracker != null) {
-				tracker.sendEvent("Devices", "Started Application",
-						deviceModel, (long) 0);
-
-			}
-		}
-	}
-
-	/**
-	 * 
-	 */
-	protected void installAnalytics() {
-		EasyTracker.getInstance().setContext(this);
-	}
-
-	/**
-	 * 
-	 */
-	protected void initializePlexappFactory() {
-		IConfiguration config = ServerConfig.getInstance(this);
-		plexFactory = PlexappFactory.getInstance(config);
-	}
-
-	public static PlexappFactory getPlexFactory() {
-		return plexFactory;
-	}
-
-	public static boolean isGoogleTV(Context context) {
-		final PackageManager pm = context.getPackageManager();
-		return pm.hasSystemFeature(COM_GOOGLE_ANDROID_TV);
-	}
-
-	public static boolean isAndroidTV(Context context) {
-		final PackageManager pm = context.getPackageManager();
-
-		if (Build.MODEL.startsWith("AFT")
-				&& Build.MANUFACTURER.equals("Amazon")) {
-			return true;
-		}
-		return pm.hasSystemFeature("android.hardware.type.television");
-	}
-
-	public static ConcurrentHashMap<String, Server> getPlexMediaServers() {
-		return plexmediaServers;
-	}
-
-	public static ImageLoader getImageLoader() {
-		return imageLoader;
-	}
-
-	/**
-	 * Retrieves the video playback queue. Items will be added and removed from
-	 * the queue and used by the video player for playback of Episodes and
-	 * Movies.
-	 * 
-	 * When an episode is finished playing it should be removed from the queue.
-	 * 
-	 * This queue is thread safe.
-	 * 
-	 * @return
-	 */
-	public static LinkedList<VideoContentInfo> getVideoPlaybackQueue() {
-		return videoQueue;
-	}
-
-	/**
-	 * Checks if the app is running on OUYA.
-	 * 
-	 * @return true if the app is running on OUYA
-	 */
-	public static boolean isRunningOnOUYA() {
-		if ("OUYA".equals(Build.MANUFACTURER)) {
-			return true;
-		}
-		return false;
+	public static void disableTracking() {
+		enableTracking = false;
 	}
 
 	public static void displayImage(String imageUrl, ImageView view) {
@@ -275,11 +110,173 @@ public class SerenityApplication extends Application {
 		imageLoader.displayImage(imageUrl, imageViewAware, options);
 	}
 
-	public static void disableTracking() {
-		enableTracking = false;
+	public static ImageLoader getImageLoader() {
+		return imageLoader;
+	}
+
+	public static DisplayImageOptions getMovieOptions() {
+		return movieOptions;
+	}
+
+	public static DisplayImageOptions getMusicOptions() {
+		return musicOptions;
+	}
+
+	public static List<PendingDownload> getPendingDownloads() {
+		return pendingDownloads;
+	}
+
+	public static PlexappFactory getPlexFactory() {
+		return plexFactory;
+	}
+
+	public static ConcurrentHashMap<String, Server> getPlexMediaServers() {
+		return plexmediaServers;
+	}
+
+	public static DisplayImageOptions getReflectiveOptions() {
+		return reflectiveOptions;
+	}
+
+	/**
+	 * Retrieves the video playback queue. Items will be added and removed from
+	 * the queue and used by the video player for playback of Episodes and
+	 * Movies.
+	 * 
+	 * When an episode is finished playing it should be removed from the queue.
+	 * 
+	 * This queue is thread safe.
+	 * 
+	 * @return
+	 */
+	public static LinkedList<VideoContentInfo> getVideoPlaybackQueue() {
+		return videoQueue;
+	}
+
+	public static boolean isAndroidTV(Context context) {
+		final PackageManager pm = context.getPackageManager();
+
+		if (Build.MODEL.startsWith("AFT")
+				&& Build.MANUFACTURER.equals("Amazon")) {
+			return true;
+		}
+		return pm.hasSystemFeature("android.hardware.type.television");
+	}
+
+	public static boolean isGoogleTV(Context context) {
+		final PackageManager pm = context.getPackageManager();
+		return pm.hasSystemFeature(COM_GOOGLE_ANDROID_TV);
+	}
+
+	/**
+	 * Checks if the app is running on OUYA.
+	 * 
+	 * @return true if the app is running on OUYA
+	 */
+	public static boolean isRunningOnOUYA() {
+		if ("OUYA".equals(Build.MANUFACTURER)) {
+			return true;
+		}
+		return false;
 	}
 
 	public static boolean isTrackingEnabled() {
 		return enableTracking;
+	}
+
+	/**
+	 * 
+	 */
+	public SerenityApplication() {
+		pendingDownloads = new ArrayList<PendingDownload>();
+
+	}
+
+	protected void configureImageLoader() {
+		DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+				.cacheInMemory(true).cacheOnDisc(true)
+				.bitmapConfig(Bitmap.Config.RGB_565)
+				.resetViewBeforeLoading(true)
+				.showImageForEmptyUri(R.drawable.default_video_cover).build();
+
+		musicOptions = new DisplayImageOptions.Builder().cacheInMemory(true)
+				.cacheOnDisc(true).bitmapConfig(Bitmap.Config.RGB_565)
+				.showImageForEmptyUri(R.drawable.default_music).build();
+
+		movieOptions = new DisplayImageOptions.Builder().cacheInMemory(true)
+				.cacheOnDisc(true).bitmapConfig(Bitmap.Config.RGB_565)
+				.resetViewBeforeLoading(true).build();
+
+		reflectiveOptions = new DisplayImageOptions.Builder()
+				.cacheInMemory(true).cacheOnDisc(true)
+				.bitmapConfig(Bitmap.Config.RGB_565)
+				.displayer(new RoundedBitmapDisplayer(10)).build();
+
+		ImageLoaderConfiguration imageLoaderconfig = new ImageLoaderConfiguration.Builder(
+				this)
+				.threadPoolSize(5)
+				.tasksProcessingOrder(QueueProcessingType.FIFO)
+				.imageDownloader(
+						new OKHttpImageLoader(this, new OkHttpClient()))
+				.defaultDisplayImageOptions(defaultOptions).build();
+
+		imageLoader = ImageLoader.getInstance();
+		imageLoader.init(imageLoaderconfig);
+	}
+
+	/**
+	 * 
+	 */
+	public void init() {
+		configureImageLoader();
+		initializePlexappFactory();
+		if (enableTracking) {
+			installAnalytics();
+		}
+		sendStartedApplicationEvent();
+	}
+
+	/**
+	 * 
+	 */
+	protected void initializePlexappFactory() {
+		IConfiguration config = ServerConfig.getInstance(this);
+		plexFactory = PlexappFactory.getInstance(config);
+	}
+
+	/**
+	 * 
+	 */
+	protected void installAnalytics() {
+		EasyTracker.getInstance().setContext(this);
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		init();
+		PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor editor = prefs.edit();
+		if (isGoogleTV(this) || isAndroidTV(this)) {
+			editor.putBoolean("serenity_tv_mode", true);
+			editor.apply();
+		}
+	}
+
+	/**
+	 * @param deviceModel
+	 */
+	protected void sendStartedApplicationEvent() {
+		String deviceModel = android.os.Build.MODEL;
+		if (enableTracking) {
+			Tracker tracker = EasyTracker.getTracker();
+			if (tracker != null) {
+				tracker.sendEvent("Devices", "Started Application",
+						deviceModel, (long) 0);
+
+			}
+		}
 	}
 }
