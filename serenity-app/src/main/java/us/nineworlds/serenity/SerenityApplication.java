a1c9badef9f29c2e85534027adbfeba88a398a9a
David Carver <d_a_carver@yahoo.com>
2014-06-06 10:16:42 -0400

    Upgrade to Universal Image Loader 1.9.2

--- serenity-app/src/main/java/us/nineworlds/serenity/SerenityApplication.java
+++ serenity-app/src/main/java/us/nineworlds/serenity/SerenityApplication.java
@@ -49,10 +49,10 @@ import com.google.analytics.tracking.android.Tracker;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.display.RoundedVignetteBitmapDisplayer;
 import com.nostra13.universalimageloader.core.imageaware.ImageViewAware;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.squareup.okhttp.OkHttpClient;
 
 /**
