3e9dccc0ae0e8274ae7b7ddd5964d02dc901bdd2
David Carver <d_a_carver@yahoo.com>
2014-08-08 09:59:43 -0400

    Issue #259: Migrate to YouTube API v3.
    Migrate to YouTube api v3, since v2 is deprecated.  This requires new libraries
    and new changes to the code.  There is an API quota of 50 million requests.
    Unless the app takes off I doubt that we ever hit that limit.

---
+++ serenity-app/src/main/java/us/nineworlds/serenity/core/TrailersYouTubeSearch.java
@@ -0,0 +1,98 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2012 David Carver
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package us.nineworlds.serenity.core;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+
+import us.nineworlds.serenity.core.model.impl.YouTubeVideoContentInfo;
+import android.util.Log;
+
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+/**
+ * @author davidcarver
+ *
+ */
+public class TrailersYouTubeSearch {
+
+	private static HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+	private static JsonFactory JSON_FACTORY = new JacksonFactory();
+	private static String API_KEY = "AIzaSyBaVoUqzjCUOwxaYAz7yQDXIJzUyBQSako";
+	protected YouTubeVideoContentInfo videoInfo = new YouTubeVideoContentInfo();
+
+	private class DoNothingHttpRequestInitializer implements
+			HttpRequestInitializer {
+
+		@Override
+		public void initialize(HttpRequest arg0) throws IOException {
+
+		}
+
+	}
+
+	public YouTubeVideoContentInfo searchForVideo(String videoTitle) {
+		try {
+			YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+					new DoNothingHttpRequestInitializer()).setApplicationName(
+					"com.nineworlds.Serenity").build();
+
+			YouTube.Search.List search = youtube.search().list("id,snippet");
+			search.setKey(API_KEY);
+			search.setType("videoes");
+			search.setMaxResults(1l);
+			search.setSafeSearch("none");
+			search.setQ(videoTitle);
+			search.setOrder("relevance");
+
+			SearchListResponse searchListResponse = search.execute();
+			List<SearchResult> searchResultList = searchListResponse.getItems();
+			if (searchResultList != null && !searchResultList.isEmpty()) {
+				SearchResult videoFeed = searchResultList.get(0);
+				videoInfo = new YouTubeVideoContentInfo();
+				ResourceId videoId = videoFeed.getId();
+
+				videoInfo.setId(videoId.getVideoId());
+				return videoInfo;
+			}
+
+		} catch (MalformedURLException e) {
+			Log.e("YouTubeSearch", e.getMessage(), e);
+		} catch (IOException e) {
+			Log.e("YouTubeSearch", e.getMessage(), e);
+		}
+		return null;
+	}
+
+}
--- serenity-app/src/main/java/us/nineworlds/serenity/core/services/YouTubeTrailerSearchIntentService.java
+++ serenity-app/src/main/java/us/nineworlds/serenity/core/services/YouTubeTrailerSearchIntentService.java
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
@@ -23,21 +23,8 @@
 
 package us.nineworlds.serenity.core.services;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import us.nineworlds.serenity.core.TrailersYouTubeSearch;
 import us.nineworlds.serenity.core.model.impl.YouTubeVideoContentInfo;
-
-import com.google.gdata.client.Query;
-import com.google.gdata.client.youtube.YouTubeQuery;
-import com.google.gdata.client.youtube.YouTubeService;
-import com.google.gdata.client.youtube.YouTubeQuery.OrderBy;
-import com.google.gdata.data.Category;
-import com.google.gdata.data.youtube.VideoEntry;
-import com.google.gdata.data.youtube.VideoFeed;
-import com.google.gdata.data.youtube.YouTubeNamespace;
-import com.google.gdata.util.ServiceException;
-
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
@@ -46,18 +33,12 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.util.Log;
 
-/**
- * @author dcarver
- *
- */
 public class YouTubeTrailerSearchIntentService extends IntentService {
 
-	private String videoTitle;
 	private static final String TRAILER = " Offical Trailer";
-	private boolean trailerCategory = true;
-	private static final String YTFEED = "http://gdata.youtube.com/feeds/api/videos";
+
 	protected YouTubeVideoContentInfo videoInfo = new YouTubeVideoContentInfo();
-	
+
 	/**
 	 * @param name
 	 */
@@ -65,24 +46,28 @@ public class YouTubeTrailerSearchIntentService extends IntentService {
 		super("YouTubeTrailerSearchIntentService");
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 *
 	 * @see android.app.IntentService#onHandleIntent(android.content.Intent)
 	 */
 	@Override
-	protected void onHandleIntent(Intent intent) {
+	public void onHandleIntent(Intent intent) {
 		String title = intent.getStringExtra("videoTitle");
 		String year = intent.getStringExtra("year");
 		String show = intent.getStringExtra("show");
 		String season = intent.getStringExtra("season");
 		String episodeNum = intent.getStringExtra("episodeNum");
+		String videoTitle;
 		if (show == null) {
 			videoTitle = "\"" + title + "\"" + TRAILER + " HD" + " " + year;
-			trailerCategory = true;
 		} else {
-			trailerCategory = false;
-			videoTitle = "\"" + show + "\" " + season + " " + episodeNum + " Promo";
+			videoTitle = "\"" + show + "\" " + season + " " + episodeNum
+					+ " Promo";
 		}
-		searchForVideo();
+		TrailersYouTubeSearch trailersSearch = new TrailersYouTubeSearch();
+
+		videoInfo = trailersSearch.searchForVideo(videoTitle);
 		sendMessageResults(intent);
 	}
 
@@ -98,37 +83,6 @@ public class YouTubeTrailerSearchIntentService extends IntentService {
 				Log.e(getClass().getName(), "Unable to send message", ex);
 			}
 		}
-		
-	}
-	
-	protected void searchForVideo() {
-		try {
-			YouTubeService service = new YouTubeService("com.nineworlds.Serenity");
-			YouTubeQuery query = new YouTubeQuery(new URL(YTFEED));
-			query.setOrderBy(OrderBy.RELEVANCE);
-			query.setFullTextQuery(videoTitle);
-			query.setSafeSearch(YouTubeQuery.SafeSearch.NONE);
-			query.setMaxResults(1);
-			Query.CategoryFilter category = new Query.CategoryFilter();
-			if (trailerCategory) {
-				category.addCategory(new Category(YouTubeNamespace.KEYWORD_SCHEME, "trailer"));
-				query.addCategoryFilter(category);
-			}
-						
-			VideoFeed videoFeed = service.query(query, VideoFeed.class);
-			if (!videoFeed.getEntries().isEmpty()) {
-				VideoEntry entry = videoFeed.getEntries().get(0);
-				videoInfo = new YouTubeVideoContentInfo();
-				videoInfo.setId(entry.getMediaGroup().getVideoId());
-			}
-						 
-		} catch (MalformedURLException e) {
-			Log.e("YouTubeSearch", e.getMessage(), e);
-		} catch (IOException e) {
-			Log.e("YouTubeSearch", e.getMessage(), e);
-		} catch (ServiceException e) {
-			Log.e("YouTubeSearch", e.getMessage(), e);
-		}
-	}
 
+	}
 }
