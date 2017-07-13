e13acfc88baad58b3f9665f690b6cdeb437d2de5
David Carver <d_a_carver@yahoo.com>
2017-07-12 20:47:13 -0400

    Issue 337 additional tweaks to correctly ignore viewstate

--- serenity-app/src/main/java/us/nineworlds/serenity/ui/browser/tv/TVShowBrowserPresenter.java
+++ serenity-app/src/main/java/us/nineworlds/serenity/ui/browser/tv/TVShowBrowserPresenter.java
@@ -94,6 +94,4 @@ public class TVShowBrowserPresenter extends MvpPresenter<TVShowBrowserView> {
         getViewState().populateSecondaryCategories(secondaryCategories, event.getCategory());
     }
 
-
-
 }
