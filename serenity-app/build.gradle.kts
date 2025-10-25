4c20d1b36a2a396ae73b1a8a67dd880d99135e96
David Carver <d_a_carver@yahoo.com>
2025-10-25 19:59:02 -0400

    feat(build): upgrade agp and dependencies
    Upgrades Android Gradle Plugin to 8.13.0 and other dependencies to their latest stable versions.The following dependency versions have been updated:
    - AGP: 8.13.0
    - com.google.gms.google-services: 4.4.4
    - com.google.firebase:firebase-crashlytics-gradle: 3.0.6
    - org.sonarsource.scanner.gradle:sonarqube-gradle-plugin: 3.3
    - AndroidX Libraries:
      - appcompat: 1.7.1
      - constraintlayout: 2.2.1
      - fragment: 1.8.9
      - junit: 1.3.0
      - leanback: 1.2.0
      - lifecycle: 2.9.4
      - test-core: 1.7.0
      - recyclerview: 1.4.0

--- serenity-app/build.gradle.kts
+++ serenity-app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   id("kotlin-allopen")
   id("org.sonarqube")
   id("com.google.firebase.crashlytics")
-  id("com.google.gms.google-services") version "4.4.0"
+  id("com.google.gms.google-services") version "4.4.4"
 }
 
 apply(from = "../jacoco.gradle")
