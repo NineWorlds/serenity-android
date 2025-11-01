cbe04b31153aa0869208b0cbc2b2de609bb371f9
David Carver <d_a_carver@yahoo.com>
2025-11-01 18:53:16 -0400

    chore: Converted BackgroundBitmapDisplayer to Kotlin
    * Updated to non deprecated preferences and drawable
    * Code converted from java to kotlin

---
+++ serenity-app/src/main/java/us/nineworlds/serenity/core/imageloader/BackgroundBitmapDisplayer.kt
@@ -0,0 +1,59 @@
+package us.nineworlds.serenity.core.imageloader
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.TransitionDrawable
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
+
+class BackgroundBitmapDisplayer(
+  private val bitmap: Bitmap?,
+  private val defaultImageId: Int,
+  private val backgroundView: View,
+) : Runnable {
+
+  override fun run() {
+    if (bitmap == null) {
+      backgroundView.setBackgroundResource(defaultImageId)
+      return
+    }
+
+    val preferences = PreferenceManager.getDefaultSharedPreferences(backgroundView.context)
+    val shouldFadeIn = preferences.getBoolean("animation_background_fadein", false)
+
+    val bitmapDrawable = BitmapDrawable(backgroundView.context.resources, bitmap)
+
+    if (!shouldFadeIn) {
+      backgroundView.background = bitmapDrawable
+      return
+    }
+
+    crossfadeImages(bitmapDrawable)
+  }
+
+  private fun crossfadeImages(newBitmapDrawable: BitmapDrawable) {
+    var currentDrawable: Drawable? = backgroundView.background
+    if (currentDrawable is TransitionDrawable) {
+      currentDrawable = currentDrawable.getDrawable(1)
+    }
+
+    if (currentDrawable == null) {
+      currentDrawable = ContextCompat.getDrawable(backgroundView.context, defaultImageId)
+    }
+
+    val drawables = arrayOf(currentDrawable, newBitmapDrawable)
+    val transitionDrawable = TransitionDrawable(drawables)
+    backgroundView.background = transitionDrawable
+    transitionDrawable.isCrossFadeEnabled = true
+    var crossFadeDuration = 200
+    if (currentDrawable is BitmapDrawable) {
+      if (currentDrawable.bitmap == newBitmapDrawable.bitmap) {
+        transitionDrawable.isCrossFadeEnabled = false
+        crossFadeDuration = 0
+      }
+    }
+    transitionDrawable.startTransition(crossFadeDuration)
+  }
+}
--- serenity-app/src/test/kotlin/us/nineworlds/serenity/core/imageloader/BackgroundBitmapDisplayerTest.kt
+++ serenity-app/src/test/kotlin/us/nineworlds/serenity/core/imageloader/BackgroundBitmapDisplayerTest.kt
@@ -1,33 +1,10 @@
-/**
- * The MIT License (MIT)
- * Copyright (c) 2012 David Carver
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
- * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package us.nineworlds.serenity.core.imageloader
 
 import android.graphics.Bitmap
 import android.graphics.drawable.TransitionDrawable
-import android.preference.PreferenceManager
 import android.view.View
 import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import androidx.test.core.app.ApplicationProvider
 import assertk.assertThat
 import assertk.assertions.isInstanceOf
@@ -43,7 +20,6 @@ import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.LooperMode
 import us.nineworlds.serenity.R
-import us.nineworlds.serenity.core.imageloader.BackgroundBitmapDisplayer
 
 @RunWith(RobolectricTestRunner::class)
 @LooperMode(LooperMode.Mode.LEGACY)
